de46c44065018af8d2694f890da80b51589e4098
NICE_hwlee <nicehwlee21@gmail.com>
2019-05-09 17:33:19 +0900

    rx기본

--- RXSample/ViewController.swift
+++ RXSample/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var button1: UIButton!
+    
+    let disposeBag = DisposeBag()
+    
+    var shownCities = [String]()
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchBar
+            .rx.text
+            .orEmpty
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] query in
+                print(query)
+                self.shownCities = self.allCities.filter({$0.hasPrefix(query)})
+                self.tableView.reloadData()
+            })
+        .dispose()
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+        cell.textLabel?.text = shownCities[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
